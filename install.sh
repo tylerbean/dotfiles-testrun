a09cbd335db92e15dd42ac3ae508b9cbc2463438
tylerbean <tyler.j.bean@gmail.com>
2023-06-16 12:53:01 -0500

    add bootloader

--- install.sh
+++ install.sh
@@ -256,7 +256,11 @@ BINARIES=()
 FILES=()
 HOOKS=(base consolefont udev autodetect modconf block encrypt filesystems keyboard)
 EOF
-arch-chroot /mnt mkinitcpio -p linux
+arch-chroot /mnt bootctl --path=/boot install
+arch-chroot /mnt bash -c "echo -e 'default arch-g14.conf\ntimeout 3\neditor 0 > /boot/loader/loader.conf"
+arch-chroot /mnt bash -c "echo -e 'title    Arch Linux\nlinux     /vmlinuz-linux\ninitrd    /amd-ucode.img\ninitrd    /initramfs-linux.img > /boot/loader/entries/arch.conf"
+arch-chroot /mnt bash -c "echo -e 'title    Arch Linux G14 Kernel\nlinux     /vmlinuz-linux-g14\ninitrd    /amd-ucode.img\ninitrd    /initramfs-linux-g14.img > /boot/loader/entries/arch-g14.conf"
+arch-chroot /mnt mkinitcpio -P
 arch-chroot /mnt pacman -Sy --noconfirm acpid
 arch-chroot /mnt systemctl enable acpid
 
