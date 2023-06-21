f1fa9aa7af4bad992399ddd49fcc1f118ec992cd
Tyler Bean <tyler.j.bean@gmail.com>
2023-06-21 14:12:40 -0500

    fix mounts

--- install.sh
+++ install.sh
@@ -166,9 +166,10 @@ btrfs subvolume create /mnt/logs
 btrfs subvolume create /mnt/temp
 btrfs subvolume create /mnt/swap
 btrfs subvolume create /mnt/snapshots
-umount /mnt
+umount /mnt/efi /mnt
 
 mount -o noatime,nodiratime,compress=zstd,subvol=root /dev/mapper/luks /mnt
+mount "${part_boot}" /mnt/efi
 mkdir -p /mnt/{mnt/btrfs-root,home,var/{cache/pacman,log,tmp,lib/{aurbuild,archbuild,docker}},swap,.snapshots}
 mount -o noatime,nodiratime,compress=zstd,subvol=/ /dev/mapper/luks /mnt/mnt/btrfs-root
 mount -o noatime,nodiratime,compress=zstd,subvol=home /dev/mapper/luks /mnt/home
